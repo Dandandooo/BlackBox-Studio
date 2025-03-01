fa7e6e5031ec259ebb5ec6ba7306067e858b672a
Daniel Philipov <batkodanny@gmail.com>
2025-02-28 19:29:47 -0600

    moved

---
+++ rust-wasm/src/blocks.rs
@@ -0,0 +1,33 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum Language {
+    Rust,
+    Python,
+    C,
+    Cpp,
+    JavaScript
+}
+
+struct Argument {
+    
+}
+
+struct Data<T> {
+    dtype: String,
+    value: T
+}
+
+struct Block {
+    lang: Language,
+    input: Vec<Argument>,
+    code: String,
+}
+
+impl Clone for Block {
+    fn clone(&self) -> Block {
+        Block {
+            lang: self.lang.clone(),
+            input: self.input.clone(),
+            code: self.code.clone(),
+        }
+    }
+}
