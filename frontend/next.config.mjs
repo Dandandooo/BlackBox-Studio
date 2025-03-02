9cc91654259499a6ac8106f812d62ab69d984570
roshan <roshanahegde@gmail.com>
2025-03-02 00:13:16 -0600

    cleaned up useless front end

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  webpack: (config) => {
-    config.target = 'electron-renderer';
-    return config;
-  },
-};
