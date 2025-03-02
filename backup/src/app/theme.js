6cf1499c9fc79b69e11b82a5a6b0792e0d363abe
Daniel Philipov <batkodanny@gmail.com>
2025-03-01 23:52:30 -0600

    we don't need these

---
+++ backup/src/app/theme.js
@@ -0,0 +1,27 @@
+export const colors = {
+  red: "#eb7184",
+  orange: "#f38d70",
+  orangeDark: "#e17050",
+  orangeBorder: "#a86145",
+  yellow: "#f1cd7a",
+  green: "#adda78",
+  greenDark: "#6a8745",
+  blue: "#6ab0f3",
+  blueDark: "#2C7FFF",
+  cyan: "#85dacc",
+  white: "#ffffff",
+  black: "#000000",
+};
+
+export const theme = {
+  primary: colors.orange,
+  primaryDark: colors.orangeDark,
+  primaryBorder: colors.orangeBorder,
+  success: colors.green,
+  successDark: colors.greenDark,
+  danger: colors.red,
+  dangerBorder: "#a84145",
+  text: colors.white,
+};
+
+export default theme;
