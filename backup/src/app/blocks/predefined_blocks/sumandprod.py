c8be2987a09c303de84c83e1f2696a1ef11c1535
faumben <48180760+faumben@users.noreply.github.com>
2025-03-02 06:58:44 -0500

    newsumandprod

--- backup/src/app/blocks/predefined_blocks/sumandprod.py
+++ backup/src/app/blocks/predefined_blocks/sumandprod.py
@@ -4,11 +4,13 @@ import json
 def calculate_sum_and_product(inputs):
     x = inputs[0]
     y = inputs[1]
-    result_sum = x + y
-    result_product = x * y
-    return [result_sum, result_product]
+    return [x + y, x * y]
 
-json_str = sys.argv[1]
+if not sys.stdin.isatty():
+    json_str = sys.stdin.read()
+else:
+    json_str = sys.argv[1]
+
 inputs = json.loads(json_str)
 output = calculate_sum_and_product(inputs)
 print(json.dumps(output))
